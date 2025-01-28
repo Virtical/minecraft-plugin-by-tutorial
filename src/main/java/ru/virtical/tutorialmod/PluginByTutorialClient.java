62c73cf10ff0270ae475f9f48488d36a7fc3f226
Virtical <138823554+Virtical@users.noreply.github.com>
2025-01-28 22:00:41 +0500

    Workspace Setup | #1

---
+++ src/main/java/ru/virtical/tutorialmod/PluginByTutorialClient.java
@@ -0,0 +1,10 @@
+package ru.virtical.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PluginByTutorialClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/ru/virtical/tutorialmod/PluginByTutorialDataGenerator.java
@@ -0,0 +1,11 @@
+package ru.virtical.tutorialmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PluginByTutorialDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
